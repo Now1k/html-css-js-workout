a8596796bf39577e8a636083f107ea651a1693fb
nowik <nowik249@gmail.com>
2023-06-30 20:24:37 +0200

    Color Change works

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Hello from Nerdbord!')
+
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+
+
+
+const changeColor = () => {
+    const box = document.querySelector(".box").style.borderColor = `#${randomColor}`
+}
+const startChange = () => {
+    setInterval(changeColor(), 3000)
+}
+
+startChange()

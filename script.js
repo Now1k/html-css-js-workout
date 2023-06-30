da50b739446477056cb481e9d5f1e2f275fbb0ee
nowik <nowik249@gmail.com>
2023-06-30 20:28:41 +0200

    Edited changeColor func.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 console.log('Hello from Nerdbord!')
 
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-
-
-
-
 const changeColor = () => {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
     const box = document.querySelector(".box").style.borderColor = `#${randomColor}`
 }
+
 const startChange = () => {
-    setInterval(changeColor(), 3000)
+    setInterval(changeColor, 3000)
 }
 
 startChange()
